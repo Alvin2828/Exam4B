ed5357cc2f95172c3e8bb4584cc36bc5d44558dd
Mason <mdurhama@gmail.com>
2025-05-05 13:01:50 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+<script>
+import Nav from './components/Nav.vue'
+export default {
+components: { Nav },
+
+data()
+{
+  return {
+    plants:
+    [
+      {id: 1, name: "Snake Plant", type:"Indoor", instock:true},
+      {id: 1, name: "Rose", type:"Outdoor", instock:false}
+    ],
+    filterType: 'All'
+  }
+},
+methods: {
+  handleFilter(type){
+  this.filterType = type;
+  console.log(this.filterType);
+  }
+}
+
+}
+
+</script>
+<template>
+  <div>
+    <Nav @filter-plants="handleFilter"/>
+  <h1>Plant Store App</h1>
+  <router-view :plants="plants" :filterType="filterType"/>
+  </div>
+</template>
+
+
+
---
+++ src/views/PlantList.vue
@@ -0,0 +1,14 @@
+<script>
+import PlantCard from '../components/PlantCard.vue';
+export default{
+    props:['plants', 'filterType'],
+    components: {PlantCard}
+}
+</script>
+<template>
+   <div v-for="plant in plants" :key="plant.id">
+    <PlantCard v-if="filterType === 'All' || plant.type === filterType"
+    :plant="plant"/>
+   </div>
+</template>
+
